d463eed047391a456b7d0fa41558af7c68ff1cf0
Har1sh-k <hkolla03@gmail.com>
2025-05-22 15:01:21 -0500

    update splitter call

--- src/preprocess/code_getter.py
+++ src/preprocess/code_getter.py
@@ -1,10 +1,11 @@
 from status_logger import logger
+from preprocess.code_splitter import split_code
 
 def reader(file_path):
     try:
         with open(file_path, 'r') as file:
             content = file.read()
-            # call splitter function here
+            split_codes = split_code(content)
     except FileNotFoundError:
         logger("error", f"File not found: {file_path}")
     except Exception as e:
