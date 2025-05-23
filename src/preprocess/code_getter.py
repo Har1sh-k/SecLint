9f1aa52ce1008e0e4b855d5a466db5aedfa1db76
Har1sh-k <hkolla03@gmail.com>
2025-05-23 13:37:46 -0500

    parsing filename

--- src/preprocess/code_getter.py
+++ src/preprocess/code_getter.py
@@ -1,11 +1,14 @@
 from status_logger import logger
 from preprocess.code_splitter import split_code
+import os
 
 def reader(file_path):
     try:
         with open(file_path, 'r') as file:
             content = file.read()
-            split_codes = split_code(content)
+            file_name = os.path.basename(file_path)
+            split_codes = split_code(content, file_name)
+            
     except FileNotFoundError:
         logger("error", f"File not found: {file_path}")
     except Exception as e:
