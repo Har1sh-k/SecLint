7ac9d568e94468b78d898fbf860eefccfceeb794
Har1sh-k <hkolla03@gmail.com>
2025-05-23 13:37:30 -0500

    splitter updated with line numbers, metadata

--- src/preprocess/code_splitter.py
+++ src/preprocess/code_splitter.py
@@ -1,27 +1,54 @@
 import ast
+from copy import deepcopy
 
-def split_code(code: str):
+def split_code(code: str, file_name: str):
 
     tree = ast.parse(code)
-
-    chunks = []
-    global_vars = []
+    
+    code_chunks = {
+        "metadata": {
+                "name": None,
+                "type": None,
+                'file_name': file_name,
+            },
+        "start_line": None,
+        "end_line": None,
+        "code": None,
+        
+    }
+    slices = []
+    global_vars = ""
     
     for node in tree.body:
+        
+        chunk = deepcopy(code_chunks)
         
         if isinstance(node, (ast.FunctionDef, ast.AsyncFunctionDef, ast.ClassDef)):
             code_chunk = ast.unparse(node)
             clean_code = "\n".join(l for l in code_chunk.splitlines() if l.strip())
-            chunks.append(clean_code)
+            chunk["code"] = clean_code
+            chunk["start_line"] = node.lineno
+            chunk["end_line"] = node.end_lineno
+            chunk["metadata"] = {
+                "name": node.name,
+                "type": type(node).__name__
+            }
+            
             
         elif isinstance(node, ast.Import) or isinstance(node, ast.ImportFrom):
             import_code = ast.get_source_segment(code, node)
-            chunks.append(import_code)
+            chunk["code"] = import_code
+            chunk["start_line"] = node.lineno
+            chunk["end_line"] = node.end_lineno
+            chunk["metadata"] = {
+                "name": node.__class__.__name__,
+                "type": type(node).__name__
+            }
             
-        elif isinstance(node, (ast.Assign, ast.AnnAssign, ast.AugAssign)):
+        elif isinstance(node, (ast.Assign, ast.AnnAssign, ast.AugAssign, ast.Global)):
             assign_code = ast.get_source_segment(code, node)
-            global_vars.append(assign_code)
-            
+            global_vars += assign_code + "\n"
+                      
         elif (
             isinstance(node, ast.If)
             and isinstance(node.test, ast.Compare)
@@ -33,9 +60,25 @@ def split_code(code: str):
         ):
             code_chunk = ast.unparse(node)
             clean_code = "\n".join(l for l in code_chunk.splitlines() if l.strip())
-            chunks.append(clean_code)   
+            chunk["code"] = clean_code
+            chunk["start_line"] = node.lineno
+            chunk["end_line"] = node.end_lineno
+            chunk["metadata"] = {
+                "name": node.__class__.__name__,
+                "type": type(node).__name__
+            }   
         else:
             print(f"Skipping node of type {type(node).__name__}")
-    chunks.append(global_vars)
+        if chunk["code"] is not None:
+            slices.append(chunk)
+            
+    if global_vars:
+        chunk["code"] = global_vars
+        chunk["start_line"] = 0
+        chunk["end_line"] = 0
+        chunk["metadata"] = {
+                "name": node.__class__.__name__,
+                "type": "Gloal variable or assignment"
+            }
     
-    return chunks
+    return slices
