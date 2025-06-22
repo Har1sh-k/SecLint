f1b260e1992ac14cd8cb4c1096cf93836cc3fab8
Har1sh-k <harish.kolla@ndus.edu>
2025-06-22 00:03:07 -0500

    update insecure data parsing

--- src/load_kb.py
+++ src/load_kb.py
@@ -3,6 +3,8 @@ from langchain_community.document_loaders import TextLoader
 from langchain_community.embeddings import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_community.document_loaders import DirectoryLoader
+from langchain_text_splitters import MarkdownHeaderTextSplitter
+from langchain.docstore.document import Document
 
 
 
@@ -15,8 +17,53 @@ def get_all_files(docs_dir):
         show_progress=True,
     )
     docs = loader.load()
-    print(f"Loaded {len(docs)} documents from {docs_dir}")
-    print(f"First document content: {docs[0].page_content[:100]}...")  
+    return docs
+
+def split_docs_h2(docs):
+    splitter = MarkdownHeaderTextSplitter(
+    headers_to_split_on=[("##", "section")],
+    strip_headers=True,     
+    return_each_line=False,
+    )
+    output=[]
+    for doc in docs:
+        chunks = splitter.split_text(doc.page_content)
+        chunks[0].metadata = {'section':"Vulnerability Name"}
+        output.append(make_vuln_doc(chunks))
+
+    return output
+
+def make_vuln_doc(chunks):
+    for chunk in chunks:
+        section = chunk.metadata.get("section", "").strip()
+        text = chunk.page_content.strip()
+        
+        if section == "Vulnerability Name":
+            vul_name = text
+        elif section == "Explanation":
+            explanation = text
+        elif section == "Secure Coding Practices (OWASP)":
+            secure_practices = text
+        elif section == "Secure Code Example":
+            secure_code_example = text
+        elif section == "References":
+            references = text
+        elif section == "Insecure Code Example":
+            insecure_code = text
+        else:
+            print(f"Unknown section: {section}")
+
+    doc= Document(
+        page_content=insecure_code,
+        metadata={
+            "Vulnerability Name": vul_name,
+            "Explanation": explanation,
+            "Secure Coding Practices": secure_practices,
+            "Secure Code Example": secure_code_example,
+            "References": references,
+        }
+    )
+    return doc
 
 
 
@@ -24,7 +71,8 @@ if __name__ == "__main__":
     base_dir = Path(__file__).parent
     docs_dir = base_dir / "security_docs"
     try:
-        get_all_files(docs_dir)
+        doc_data=get_all_files(docs_dir)
+        data=split_docs_h2(doc_data)
     except FileNotFoundError as e:
         print(f"FileNotFoundError: {e}")
     except PermissionError as e:
