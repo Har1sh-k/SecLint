c0e1c31625ca6320c4d3651a7d7788f0a0b16070
Har1sh-k <harish.kolla@ndus.edu>
2025-06-21 21:45:41 -0500

    check chunk

--- src/main.py
+++ src/main.py
@@ -5,3 +5,9 @@ from preprocess.code_getter import reader
 def main(file_path):
     logger("info", "Starting the application...")
     code_chunks=reader(file_path)
+    if code_chunks:
+        logger("info", f"Successfully processed {len(code_chunks)} code chunks.")
+    else:
+        logger("warning", "No code chunks were processed.")
+        logger("info", "Exiting the application...")
+        return
